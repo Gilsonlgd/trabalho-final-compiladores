0200c3a199889631c77800ea4ab3844b83c1ed63
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-07-13 15:42:59 -0300

    ✨ feat: Implement main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "../include/Parser.h"
+#include "../include/Interpretador.h"
+
+int main() {
+    std::string nomeArquivo = "gramatica.txt";
+    Parser parser(nomeArquivo);
+
+    if (!parser.parse()) {
+        std::cerr << "Falha ao analisar a gramatica." << std::endl;
+        return 1;
+    }
+
+    Interpretador interpretador(parser.obterGramatica());
+    std::string palavra;
+    std::cout << "Digite uma palavra para ser validada: ";
+    std::cin >> palavra;
+
+    if (interpretador.reconhecer(palavra)) {
+        std::cout << "A palavra eh valida." << std::endl;
+    } else {
+        std::cout << "A palavra nao eh valida." << std::endl;
+    }
+
+    return 0;
+}
