bf006e05d5647b37296ed44d1b49760789d5306e
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-07-13 20:52:08 -0300

    ✨ feat: Introduce multiple terminal simbols per production

--- src/Interpretador.cpp
+++ src/Interpretador.cpp
@@ -7,20 +7,29 @@ Interpretador::Interpretador(const Gramatica& gramatica) : gramatica(gramatica) 
 bool Interpretador::reconhecer(const std::string& palavra) {
     std::string atual = std::string(1, gramatica.getSimboloInicial());
     std::cout << "Simbolo inicial: " << atual << std::endl;
-    for (char ch : palavra) {
-        std::cout << "Simbolo: " << ch << std::endl;
+
+    size_t pos = 0;
+    while (pos < palavra.length()) {
+        std::cout << "Posicao: " << pos << ", Simbolo atual: " << atual << std::endl;
         bool casou = false;
         for (const auto& prod : gramatica.getProducoes()) {
-            if (prod.naoTerminal == atual.back() && prod.producao.front() == ch) {
-                std::cout << "Producao: " << prod.naoTerminal << " -> " << prod.producao << std::endl;
-                atual.pop_back();
-                atual += prod.producao.substr(1);
-                casou = true;
-                break;
+            if (prod.naoTerminal == atual.back()) {
+                std::string terminais = prod.producao.substr(0, prod.producao.length() - 1);
+                std::cout << "Tentando casar: " << terminais << std::endl;
+                char proximoNaoTerminal = prod.producao.back();
+                
+                if (palavra.substr(pos, terminais.length()) == terminais) {
+                    std::cout << "Producao: " << prod.naoTerminal << " -> " << prod.producao << std::endl;
+                    atual.pop_back();
+                    atual += proximoNaoTerminal;
+                    pos += terminais.length();
+                    casou = true;
+                    break;
+                }
             }
         }
         if (!casou) {
-            std::cerr << "Erro na posicao: " << &ch - &palavra[0] << std::endl;
+            std::cerr << "Erro na posicao: " << pos << std::endl;
             return false;
         }
     }
