86eb9a84ce7fc5f97174b775012c857750e537fe
Jhuanassumpcao <jhuanassumpcao@gmail.com>
2024-07-13 22:27:18 -0300

    feat: add message error

--- src/Interpretador.cpp
+++ src/Interpretador.cpp
@@ -1,49 +1,50 @@
 #include "../include/Interpretador.h"
 #include <iostream>
-#include <iterator> // Para std::prev
 #include <string>
 #include <queue>
-#include <unordered_set>
 
 Interpretador::Interpretador(const Gramatica &gramatica)
     : gramatica(gramatica) {}
 
 bool Interpretador::reconhecer(const std::string &palavra) {
-   // Verifica se a palavra está vazia
     if (palavra.empty()) {
+        std::cerr << "Palavra vazia." << std::endl;
         return false;
     }
 
-    // Inicializa a fila de estados
     std::queue<std::string> fila;
     fila.push(std::string(1, gramatica.getSimboloInicial()));
+
+    std::cout << "Simbolo inicial: " << gramatica.getSimboloInicial() << std::endl;
 
     while (!fila.empty()) {
         std::string estado = fila.front();
         fila.pop();
 
-        // Se o estado é igual à palavra, a palavra é reconhecida
+        std::cout << "Estado atual: " << estado << std::endl;
+
         if (estado == palavra) {
+            std::cout << "Palavra reconhecida." << std::endl;
             return true;
         }
 
-        // Se o estado é mais longo que a palavra, não é necessário continuar
         if (estado.size() > palavra.size()) {
-            continue;
+            return false;
         }
 
-        // Expande os estados atuais
         for (size_t i = 0; i < estado.size(); ++i) {
             if (gramatica.getNaoTerminais().count(estado[i])) {
                 char naoTerminal = estado[i];
                 const auto& producoes = gramatica.getProducoes();
 
-                // Verifica produções para o não terminal atual
+                std::cout << "Expandindo a partir de: " << estado[i] << std::endl;
+
                 for (const auto& producao : producoes) {
                     if (producao.naoTerminal == naoTerminal) {
                         for (const auto& alternativa : producao.producoes) {
                             std::string novoEstado = estado.substr(0, i) + alternativa + estado.substr(i + 1);
                             if (novoEstado.length() <= palavra.length()) {
+                                std::cout << "Aplicando producao: " << producao.naoTerminal << " -> " << alternativa << std::endl;
                                 fila.push(novoEstado);
                             }
                         }
@@ -53,5 +54,6 @@ bool Interpretador::reconhecer(const std::string &palavra) {
         }
     }
 
+    std::cerr << "Erro na posicao: " << palavra.length() << std::endl;
     return false;
 }
